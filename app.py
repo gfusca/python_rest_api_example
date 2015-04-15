417d2524b7a0e11d83f2be3ad01d4f536effc211
Gabriel Fusca <gabrielfusca@gmail.com>
2015-04-15 11:49:00 -0300

    Se crea la versión inicial de una REST API simple para autentificación de usuarios

--- app.py
+++ app.py
@@ -1,9 +1,57 @@
 from flask import Flask
+from flask.ext.httpauth import HTTPBasicAuth
+from flask.ext.api import status
+from json import dumps
+from flask import request
+from flask import Response
+
 app = Flask(__name__)
+
+auth = HTTPBasicAuth()
+
+users = {
+    "admin": "root",
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+@app.route('/secret')
+@auth.login_required
+def index():
+    return "Hello, %s!" % auth.username()
 
 @app.route('/')
 def root_test():
-    return 'Root!'
+    return 'index'
+
+@app.route('/user', methods=['GET'])
+def get_users():
+	return dumps(users)	
+
+@app.route('/user/<string:username>', methods=['GET'])
+def get_profile(username):
+	return username + " profile"
+
+@app.route('/user/<string:username>', methods=['POST'])
+def add_user(username):
+	users[username] = request.form['pwd']
+	resp = Response(dumps(username), status=200, mimetype='application/json')
+	return resp
+
+@app.route('/user/<string:username>/pwd', methods=['PUT'])
+def change_password(username):
+	resp = Response("Pasword changed" , status=200, mimetype='application/json')
+	if not users.has_key(username):
+		resp = Response("User " + username + " not found!" , status=403, mimetype='application/json')
+	else:
+		users[username] = request.form['pwd']
+
+	return resp
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
